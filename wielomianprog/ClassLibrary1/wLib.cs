1a4ac20fa64611a2132bd73bb10ac58a27b22585
Dietrich <kdj287zt@hotmail.com>
2022-06-26 22:44:13 +0200

    Add files via upload

---
+++ wielomianprog/ClassLibrary1/wLib.cs
@@ -0,0 +1,216 @@
+﻿namespace WielomianLib;
+public class wLib
+{
+    #region PolynomialObject
+
+    private class PolynomialObject
+    {/// <summary>
+     /// tworzy obiekt w pamięci przechowując wartości liczbowe
+     /// </summary>
+     /// <param name="value">przechowuje wartość przed X </param>
+     /// <param name="impressiveness"> przechowuje potęgę </param>
+        public PolynomialObject(int value, int impressiveness)
+        {
+            this.impressiveness = impressiveness;
+            this.value = value;
+
+        }
+        public int value { get; set; }
+        public int impressiveness { get; set; }
+
+    }
+    #endregion
+    #region addingAPolynomial
+    /// <summary>
+    /// dodaje dwa wielomiany
+    /// </summary>
+    /// <param name="xlist">pobiera pierwszy wielomian</param>
+    /// <param name="zlist"> pobiera 2 wielomian </param>
+    public void addingAPolynomial(List<int> xlist, List<int> zlist)
+    {
+        List<int> list = new List<int>();
+        //jeżeli pierwszy wielomian jest większy to wykonuje tę operację jako pierwszą
+        if (xlist.Count > zlist.Count)
+        {
+            for (int i = 0; i < xlist.Count; i++)
+            {
+                if (i < zlist.Count)
+                {
+                    list.Add(zlist.ElementAt(i) + xlist.ElementAt(i));
+                }
+                else
+                {
+                    list.Add(xlist.ElementAt(i));
+                }
+            }
+        }
+        else//jeżeli drugi wielomian jest większy to wykonuje tę operację jako pierwszą
+        {
+            for (int i = 0; i < zlist.Count; i++)
+            {
+                if (i < xlist.Count)
+                {
+                    list.Add(zlist.ElementAt(i) + xlist.ElementAt(i));
+
+                }
+                else
+                {
+                    list.Add(zlist.ElementAt(i));
+                }
+            }
+        }
+        //przekazuje wielomian do wyświetlenia
+        HumanUnderstableConverter(list);
+    }
+    #endregion
+    #region PolynomialSubtraction
+    /// <summary>
+    /// odejmuje dwa wielomiany
+    /// </summary>
+    /// <param name="xlist">pobiera pierwszy wielomian</param>
+    /// <param name="zlist"> pobiera 2 wielomian </param>
+    public void PolynomialSubtraction(List<int> xlist, List<int> zlist)
+    {
+        List<int> list = new List<int>();
+        //jeżeli pierwszy wielomian jest większy to wykonuje tę operację jako pierwszą
+        if (xlist.Count > zlist.Count)
+        {
+            for (int i = 0; i < xlist.Count; i++)
+            {
+                if (i < zlist.Count)
+                {
+                    list.Add(zlist.ElementAt(i) - xlist.ElementAt(i));
+                }
+                else
+                {
+                    list.Add(xlist.ElementAt(i));
+                }
+            }
+        }
+        else//jeżeli drugi wielomian jest większy to wykonuje tę operację jako pierwszą
+        {
+            for (int i = 0; i < zlist.Count; i++)
+            {
+                if (i < xlist.Count)
+                {
+                    list.Add(zlist.ElementAt(i) - xlist.ElementAt(i));
+
+                }
+                else
+                {
+                    list.Add(zlist.ElementAt(i));
+                }
+            }
+        }
+        //przekazuje wielomian do wyświetlenia 
+        HumanUnderstableConverter(list);
+    }
+    #endregion
+    #region MultiplicationOfAPolynomial
+    /// <summary>
+    /// pozwala na mnożenie wielomianu i zwraca jego obliczoną wartość.
+    /// </summary>
+    /// <param name="xlist">list of wielomian values</param>
+    /// <param name="zlist">list of wielomian values</param>
+    public void MultiplicationOfAPolynomial(List<int> xlist, List<int> zlist)
+    {
+
+        /*temp lists and value*/
+        IList<PolynomialObject> list = new List<PolynomialObject>();
+        IList<PolynomialObject> list2 = new List<PolynomialObject>();
+        IList<PolynomialObject> list3 = new List<PolynomialObject>();
+        IList<PolynomialObject> list4 = new List<PolynomialObject>();
+        List<int> finish = new List<int>();
+        int temp = 0;
+        /*end temp lists and value*/
+
+        // przekazywanie wartości pierwszego wielomianu do obiektu 
+        for (int i = 0; i < xlist.Count; i++)
+        {
+            list.Add(new PolynomialObject(xlist[i], i));
+        }
+        // przekazywanie wartości drugiego wielomianu do obiektu 
+        for (int i = 0; i < zlist.Count; i++)
+        {
+            list2.Add(new PolynomialObject(zlist[i], i));
+        }
+        // operacja mnożenia obiektów (wielomianów)
+        foreach (var iteml1 in list)
+        {
+            foreach (var iteml2 in list2)
+            {
+                list3.Add(new PolynomialObject(iteml1.value * iteml2.value, iteml1.impressiveness + iteml2.impressiveness));
+            }
+        }
+        //ustalanie najwyższej potęgi
+        foreach (var item3 in list3)
+        {
+            if (item3.impressiveness > temp)
+            {
+                temp = item3.impressiveness;
+            }
+        }
+        //układanie listy według potęgi dla łatwiejszej konwersji na listę typów
+        for (int i = 0; i < temp + 1; i++)
+        {
+            list4.Add(new PolynomialObject(0, i));
+        };
+
+        //łączenie wielomianów według potęgi
+        for (int i = 0; i < list4.Count; i++)
+        {
+            for (int x = 0; x < list3.Count; x++)
+            {
+                if (list3[x].impressiveness == list4[i].impressiveness)
+                {
+                    list4[i].value = list4[i].value + list3[x].value;
+                }
+            }
+        }
+        //konwersja stosu obiektów na stos wartości
+        foreach (var item in list4)
+        {
+            finish.Add(item.value);
+        }
+        //przekazanie stosu wartości do generatora czytelnego wielomianu 
+        HumanUnderstableConverter(finish);
+    }
+    #endregion
+    #region HumanUnderstableConverter
+    /// <summary>
+    /// przyjmuje wielomian w celu wypisania go w bardziej ludzkiej formie
+    /// </summary>
+    /// <param name="xlist">przyjmuje wielomian w celu wypisania go w bardziej ludzkiej formie ;) </param>
+    private void HumanUnderstableConverter(List<int> xlist)
+    {
+        Console.WriteLine("twój nowy wielomian to:");
+        var Wlenght = xlist.Count();
+        List<int> revlist = new List<int>();
+        revlist = Enumerable.Reverse(xlist).ToList();
+        for (int i = 0; i < revlist.Count - 1; i++)
+        {
+            string element;
+            if (revlist.ElementAt(i) >= 0)
+            {
+                element = "+" + revlist.ElementAt(i);
+            }
+            else
+            {
+                element = revlist.ElementAt(i).ToString();
+            }
+            Console.Write("{0}x^{1}  ", element, Wlenght - 1);
+            Wlenght--;
+        }
+        if (revlist.Last() >= 0)
+        {
+            Console.Write("+" + revlist.Last() + "x");
+        }
+        else
+        {
+            Console.Write(revlist.Last() + "x");
+        }
+    }
+
+    #endregion
+
+}
---
+++ wielomianprog/wielomianprog/Program.cs
@@ -0,0 +1,89 @@
+﻿using WielomianLib;
+wLib wLib = new wLib();
+
+
+
+
+#region pools
+//listy przyjmujące wartość wielomianu 
+List<int> polynomial1 = new List<int>();
+List<int> polynomial2 = new List<int>();
+//pobieranie wartości wielomianu
+#endregion
+#region basic stuff to work on 
+for (int wielomianNumber = 0; wielomianNumber < 2; wielomianNumber++)
+{
+ERROR1:/*wykrycie błędu w przypadku wybrania złej wartości w wyborze ilości potęg wielomianu */
+    Console.WriteLine("ilość potęg wielomianu nr {0}", wielomianNumber + 1);
+    var x = Console.ReadLine();
+    bool isnumeric = int.TryParse(x, out int q);
+    if (isnumeric)//sprawdzanie czy wartość jest numeryczna
+    {
+        for (int i = 0; i < q; i++)
+        {
+        ERROR2:/*wykrycie błędu w przypadku nie numerycznej wartości wprowadzonej  */
+            Console.WriteLine("podaj wartość dla x^{0} : ", i);
+            bool isnumericforX = int.TryParse(Console.ReadLine(), out int xvalue);//sprawdzanie czy wartość jest numeryczna
+            if (isnumericforX)
+            {
+                switch (wielomianNumber)
+                {
+                    case 0:
+                        polynomial1.Add(xvalue);
+                        break;
+                    case 1:
+                        polynomial2.Add(xvalue);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("podana wartość to nie liczba!");
+                goto ERROR2;/*wykrycie błędu w przypadku nie numerycznej wartości wprowadzonej  */
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("podana wartość to nie liczba!");
+        goto ERROR1;/*wykrycie błędu w przypadku wybrania złej wartości w wyborze ilości potęg wielomianu */
+    }
+}
+#endregion
+#region wybór operacji na bibliotece
+ERROR3:
+Console.WriteLine("wybierz operacje na której chcesz działać");
+Console.WriteLine("1 dodawanie" +
+    "2 odejmowanie " +
+    "3 mnożenie ");
+Console.ReadLine();
+bool isnumericforZ = int.TryParse(Console.ReadLine(), out int v);
+if (isnumericforZ)
+{
+    switch (v)
+    {
+        case 1:
+            wLib.addingAPolynomial(polynomial1, polynomial2);
+            break;
+        case 2:
+            wLib.PolynomialSubtraction(polynomial1, polynomial2);
+            break;
+        case 3:
+            wLib.MultiplicationOfAPolynomial(polynomial1, polynomial2);
+            break;
+        default:
+            Console.WriteLine("nieprawidłowa wartość");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("podana wartość to nie liczba!");
+    goto ERROR3;
+}
+#endregion
+
+
+
